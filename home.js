09e1b2bd26bc1e675924ff50800a9a1bd54a6ff0
Perry Hartono <phar2413@uni.sydney.edu.au>
2021-02-25 18:53:36 +1100

    Add functions for converting Latin letters into their corresponding binary codes, as well as some sample code

--- home.js
+++ home.js
@@ -59,6 +59,22 @@ function drawGridLines(ctx, sizeX, sizeY) {
   }
 }
 
+// Convert a Latin alphabet letter into its corresponding number in the Latin alphabet order (A, B, C etc)
+function letterToNum(letter) {
+  // Note: 97 = char code for the lowercase letter 'a'
+  return letter.charCodeAt(0) - 97;
+}
+
+// Convert an integer into a binary string
+function numToBin(num) {
+  return num.toString(2);
+}
+
+// Pad a binary string to the given amount of digits
+function padBinaryString(binStr, numDigits) {
+  return "0".repeat(numDigits - binStr.length) + binStr;
+}
+
 // Initialise the experimental canvas element
 function initExperiment() {
   // Add an event listener to the canvas element to detect mouse clicks
@@ -123,23 +139,52 @@ function drawExperiment() {
     ctx.fillText('They the best', 310, 70);
     ctx.font = '30px sans-serif';
     ctx.strokeText('Outlier', 310, 120);
+
+    // Sample text    
+    var sampleText = "polandcannotintospace";
+    var encodedText = sampleText.split("").map(letter => letterToNum(letter) + 1)
+                                          .map(num => numToBin(num))
+                                          .map(binStr => padBinaryString(binStr, 7));
+    console.log(encodedText);
     
     // Arcs to represent the pattern
-    var numOfArcs = 10;
+    var numOfArcs = 80;
     var arcSize = 360 / numOfArcs;
-    var overlapDegrees = 0.2;
-    var binaryMsg = "0100110011";
+    var overlapDegrees = 0.4;
+    //var binaryMsg = "1010101010";
+    var binaryMsg = "0100110000" + "0010111000" + "0000111000" + "0011010000"
+                  + "0001010000" + "1111111111" + "1111111111" + "1111111000";
     
+    var binaryMsg2 = encodedText.slice(0, 8).map(binStr => binStr + "0".repeat(3)).join("");
+    console.log(binaryMsg2);
+
     for (var i = 0; i < numOfArcs; i += 1) {
       // If char is 1, draw an arc
       if (binaryMsg[i] == 1) {
         ctx.beginPath();
         // Draw an arc clockwise.
         // Formula is (Math.PI/180)*deg where 'deg' is the angle in degrees.
-        // To make adjacent arcs merge with each other more seamlessly, an additional 0.2 degrees is added for the other side of the arc.
-        ctx.arc(300, 300, 75, (Math.PI/180)*arcSize*i, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), false);
+        // To make adjacent arcs merge with each other more seamlessly,
+        // an additional 0.2 degrees is added for the other side of the arc.
+        ctx.arc(300, 300, 70+15, (Math.PI/180)*arcSize*i, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), false);
         // Draw the other arc counterclockwise
-        ctx.arc(300, 300, 60, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), (Math.PI/180)*arcSize*i, true);
+        ctx.arc(300, 300, 70, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), (Math.PI/180)*arcSize*i, true);
+        //ctx.lineTo(550, 500);
+        ctx.fillStyle = 'rgb(255, 230, 0)';
+        ctx.fill();
+      }
+    }
+    for (var i = 0; i < numOfArcs; i += 1) {
+      // If char is 1, draw an arc
+      if (binaryMsg2[i] == 1) {
+        ctx.beginPath();
+        // Draw an arc clockwise.
+        // Formula is (Math.PI/180)*deg where 'deg' is the angle in degrees.
+        // To make adjacent arcs merge with each other more seamlessly,
+        // an additional 0.2 degrees is added for the other side of the arc.
+        ctx.arc(300, 300, 85+15, (Math.PI/180)*arcSize*i, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), false);
+        // Draw the other arc counterclockwise
+        ctx.arc(300, 300, 85, (Math.PI/180)*(arcSize*(i+1)+overlapDegrees), (Math.PI/180)*arcSize*i, true);
         //ctx.lineTo(550, 500);
         ctx.fillStyle = 'rgb(255, 230, 0)';
         ctx.fill();
